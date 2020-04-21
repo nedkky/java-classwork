c74470a6beb82a984044f09e14eb3503800a9e0e
Nedko Gospodinov <nedko.gospodinov3@gmail.com>
2020-04-21 16:01:06 +0300

    |Homework 2 / exercise 2

---
+++ homework week 2/exercise 2/src/com/company/Main.java
@@ -0,0 +1,17 @@
+package com.company;
+
+import java.util.Random;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String month = "Януари";
+        if (month == "Януари" || month == "Март" || month == "Май" || month == "Юли" || month == "Август" || month == "Октомври" || month == "Декември") {
+            System.out.println("Има 31 дни в " +month);
+        } else if ( month == "Април" || month == "Юни" || month == "Септември" || month == "Ноември") {
+            System.out.println("Има 30 дни  в " + month);
+        } else if ( month == "Февруари") {
+            System.out.println("Има 29 дни в " + month);
+        }
+    }
+}
